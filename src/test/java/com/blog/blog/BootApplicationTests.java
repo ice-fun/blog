acc9f242a10094935d76dd21c9aac8703a464cf9
xuguoxing <xuguoxing@LAPTOP-UEP31RAJ>
2020-10-21 18:05:29 +0800

    测试应用改动-添加博客用户

--- src/test/java/com/blog/blog/BootApplicationTests.java
+++ src/test/java/com/blog/blog/BootApplicationTests.java
@@ -1,8 +1,11 @@
 package com.blog.blog;
 
 import com.blog.blog.api.admin.service.AdminAdminService;
+import com.blog.blog.api.user.service.UserUserService;
 import com.blog.blog.bean.admin.po.Admin;
+import com.blog.blog.bean.user.po.User;
 import com.blog.blog.config.PropertyConfig;
+import com.blog.blog.utils.MD5Utils;
 import com.blog.blog.utils.RedisUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +23,9 @@ class BootApplicationTests {
     AdminAdminService adminService;
 
     @Resource
+    UserUserService userService;
+    
+    @Resource
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
@@ -32,5 +38,13 @@ class BootApplicationTests {
         boolean save = adminService.save(admin);
         System.out.println(save);
     }
-
+    
+    @Test
+    void addUser(){
+        User user = new User();
+        user.setUserAccount("test");
+        user.setUserPassword(bCryptPasswordEncoder.encode(MD5Utils.md5Decode32WithSalt("11111111")));
+        boolean save = userService.save(user);
+        System.out.println(save);
+    }
 }
