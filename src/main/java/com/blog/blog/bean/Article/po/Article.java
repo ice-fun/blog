5cb8f98bb1d92acc7a6713b1ee1d59990358d3bf
liyaobing <848617514@qq.com>
2020-10-21 09:55:55 +0800

    注解

--- src/main/java/com/blog/blog/bean/Article/po/Article.java
+++ src/main/java/com/blog/blog/bean/Article/po/Article.java
@@ -8,6 +8,8 @@ import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.blog.blog.bean.admin.po.Admin;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -21,6 +23,7 @@ import lombok.Data;
  * */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@TableName(autoResultMap = true) // mybatisPlus 自动映射JSON类型的sql列
 public class Article {
 	
 	@TableId
@@ -31,6 +34,8 @@ public class Article {
 	//创作类型 1原创 2转载
 	private String produceType;
 	//文章标签列表
+	// 这个注解是 选择mybatisPlus 映射JSON类型的处理类，不添加无法转换JSON数据
+	@TableField(typeHandler = JacksonTypeHandler.class)
 	private List<String> articleLabels;
 	//标题
 	private String articleTitle;
@@ -44,18 +49,18 @@ public class Article {
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "article_publish_time", fill = FieldFill.INSERT)
-	private Date articlePublishTime;//发布时间
+	private LocalDateTime articlePublishTime;//发布时间
 	
 	@TableField(value = "is_delete", fill = FieldFill.INSERT)
-	private int isDelete;
+	private Integer isDelete;
 	
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "create_time", fill = FieldFill.INSERT)
-	private Date createTime;
+	private LocalDateTime createTime;
 	
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "update_time", fill = FieldFill.INSERT)
-	private Date updateTime;
+	private LocalDateTime updateTime;
 }
